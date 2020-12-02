b11272f353f34e39b6f6f455562398405175767d
Giovanni Fantuzzi <giofantuzzi@hotmail.it>
2020-12-02 15:23:55 +0000

    Add degree-1 moment extraction util

---
+++ modules/sparsemoment/extractmomentsolution.m
@@ -0,0 +1,12 @@
+function x = extractmomentsolution(y, exponents)
+
+% x = extractmomentsolution(y, moments) reads the degree-1 monomials from a
+% moment-SOS solution y. The values in y correspond to moments with
+% exponents in the "exponents" argument.
+
+islinear = (sum(exponents,2)==1);
+[I,J] = find(exponents(islinear,:));
+[~, order] = sort(I);
+[~, order] = sort(J(order));
+x = y(islinear);
+x = x(order);
